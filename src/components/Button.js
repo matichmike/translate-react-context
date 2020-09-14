0441874a31b1c5b24cc95d09e275f1fc05bf5821
Michael Matich <matich.mike@gmail.com>
2020-09-14 11:50:39 -0400

    context provider added

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,8 +5,9 @@ class Button extends React.Component {
   static contextType = LanguageContext;
 
   render() {
+    const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
     return (
-      <button className="ui button primary">Submit</button>
+      <button className="ui button primary">{text}</button>
     )
   }
 }
